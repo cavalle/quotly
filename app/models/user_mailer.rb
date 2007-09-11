d69bbabebd760e289f903c3795ceea29fe36def7
cavalle <cavalle@bee9e211-a9ea-db11-af9a-0014220fd224>
2007-09-11 14:28:37 +0000

    updated q.uot.es url in mails
    git-svn-id: http://lmcavalle.com/svn/projects/quotes/trunk@14 bee9e211-a9ea-db11-af9a-0014220fd224

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://q.uot.es/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://q.uot.es/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "team@q.uot.es"
+      @subject     = "[q.uot.es] "
       @sent_on     = Time.now
       @body[:user] = user
     end
