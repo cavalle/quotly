f3a8ffc07483598fd7c0c127f0e476a6dc74cd15
Luismi Cavalle <luismi@lmcavalle.com>
2010-01-17 17:08:21 +0100

    Only css for focused styling

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,10 +4,4 @@ $(document).ready(function(){
   }, function(){
     $(this).find(".actions").fadeOut();
   });
-  $("#openid_identifier").focus(function(){
-    $(this).addClass("selected");
-  });
-  $("#openid_identifier").blur(function(){
-    if ($(this).val() == "") $(this).removeClass("selected");
-  })
 })
